90341f0c6576c98b267697583eb0151c174f90ab
vega2k <vega2k@naver.com>
2018-03-30 04:47:10 +0900

    ionicBasic first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LoadingProvider } from '../providers/loading/loading';
+import { SQLite } from "@ionic-native/sqlite";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +19,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
-  ],
+   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LoadingProvider,
+    SQLite,
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,70 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  disabledSwitch = false;
 
-  constructor(public navCtrl: NavController) {
+  items = [
+    {"id":1, name:"UI Component"},
+    {"id":2, name:"SQLite"},
+    {"id":3, name:"세번째 item"},
+    {"id":4, name:"네번째 item"},
+    {"id":5, name:"다섯번째 item"},
+  ];
 
+  userName : any;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  // ionViewDidLoad() {
+  //   console.log('ionViewDidLoad HomePage 1');
+  // }
+  // ionViewWillEnter() {
+  //   console.log('ionViewWillEnter HomePage 2');
+  // }
+  // ionViewDidEnter() {
+  //   console.log('ionViewDidEnter HomePage 3');
+  // }
+  // ionViewWillLeave() {
+  //   console.log('ionViewWillLeave HomePage 4');
+  // }
+  // ionViewDidLeave() {
+  //   console.log('ionViewDidLeave HomePage 5');
+  // }
+  // ionViewWillUnload() {
+  //   console.log('ionViewWillUnload HomePage 6');
+  // }
+  // ionViewCanEnter() {
+  //   console.log('ionViewCanEnter HomePage 7');
+  // }
+  // ionViewCanLeave() {
+  //   console.log('ionViewCanLeave HomePage 8');
+  // }
+
+  itemSelected(item){
+    if (item.id === 1) {
+      this.navCtrl.push('componentPage');
+    }else if ( item.id === 2) {
+      this.navCtrl.push('databasePage');
+    }
+  }
+
+  clickButton(event) {
+    console.log(event);
+    this.navCtrl.push('bindPage');
+  }
 }
